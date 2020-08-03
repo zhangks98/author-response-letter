27da671493afbf16296437a482a1429161fc2a10
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2020-08-04 01:32:36 +0800

    Modified the preamble.

--- RC_to_MS/make-md.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set NAME=Einstein1905
-pandoc -s -S --template ../templates/RC_to_MS-pandoc.tex %NAME%.md -o %NAME%.pdf
--- RC_to_MS/make-tex.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set NAME=Einstein1905
-pdflatex %NAME%.tex
-rm %NAME%.aux
-rm %NAME%.log
-rm %NAME%.out
